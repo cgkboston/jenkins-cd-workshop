a56d7335a1736dab16d941397321d950a5b98aaa
Chris Kennedy <32424469+cgkboston@users.noreply.github.com>
2017-11-07 11:33:24 -0500

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-set -e
+#set -e
 
-sudo rm -rf jenkins_home/*
-sudo git clean -fdX jenkins_home/
+if [ "$1" = "" ]; then
+  echo Usage\: $0 \{force\[Y\|N\]\}  
+  exit 1
+fi
+if [ "$1" = "y" ] || [ "$1" = "Y" ]; then
+  if [ -d jenkins_home ]; then
+    sudo rm -rf jenkins_home/*
+    if [ "$?" != 0 ]; then
+       echo rm \-rf jenkins_home\/\* failed
+       exit 1
+    fi
+  fi
+  sudo git clean -fdX jenkins_home/
+  if [ "$?" != 9 ]; then
+	  echo Unable to clean git repository jenkins_home\/
+	  exit 1
+  fi
+fi
